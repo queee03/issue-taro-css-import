c75a0211a681318140831e35d675b216ece8f0b1
yangzq <yangzq@infore.com>
2022-11-11 11:21:20 +0800

    perf

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,7 +5,9 @@ const Index = () => {
   return (
     <view>
       {APP_TITLE}
+      用相对路径引入的图片，正常显示：
       <view className={styles.bg1}></view>
+      用路径别名引入的图片，提示“本地资源图片无法通过 WXSS 获取”，无法显示：
       <view className={styles.bg2}></view>
     </view>
   );
